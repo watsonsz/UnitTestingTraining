f5d0ee96f67908d8210698746d0dc185704e0076
Zack Watson <zack.watson@cgi.com>
2024-06-13 11:37:20 -0500

    Began File Logger

---
+++ FileLoggerKata/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLoggerKata
+{
+    public class Logger
+    {
+        //realistically this would be a dependency-injected File Handler
+        private static string _FileLocation;
+        public Logger(string FileLocation)
+        {
+            if(!File.Exists(FileLocation))
+            {
+                File.Create(FileLocation);
+            }
+        }
+        public void Log(string message)
+        {
+
+        }
+    }
+}
